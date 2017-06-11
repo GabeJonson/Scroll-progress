8c3cf49438bf6bfcd4a26fe52ce498df310f9c66
Gabe Jonson <sky3107@gmail.com>
2017-06-11 17:16:47 +0300

    Made class from fnc

--- scroll.js
+++ scroll.js
@@ -1,19 +1,53 @@
-(() => {
-  const progressLine = document.getElementById('progress-line'),
-        body = document.body,
-        html = document.documentElement;
+window.onload = () => {
+  const scrollElement = new Scroll('.progress-line');
+  scrollElement.init();
+}
 
-  let viewportHeight = window.innerHeight,
-      documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight),
+class Scroll {
+  constructor(element, height = '5px', color = '#333') {
+    this.element = element;
+    this.height = height;
+    this.color = color;
+  }
 
-      scrollTop = () => scrollFromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  init() {
+    const progressLine = document.querySelector(this.element),
+          body = document.body,
+          html = document.documentElement;
 
-  window.onresize = () => documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+    let viewportHeight = window.innerHeight,
+        documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
 
-  window.onscroll = () => {
-    let scroll = scrollTop(),
+    progressLine.style.position = 'fixed';
+    progressLine.style.top = 0;
+    progressLine.style.left = 0;
+    progressLine.style.transition = '0.3s';
+
+    this.scrollTop();
+
+    window.onresize = () => {
+      this.resize(documentHeight, body, html);
+    }
+
+    window.onscroll = () => {
+      this.scroll(progressLine, documentHeight, viewportHeight);
+    }
+  }
+
+  scrollTop(scrollFromTop) {
+    return scrollFromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+  }
+
+  resize(documentHeight, body, html) {
+    return documentHeight = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+  }
+
+  scroll(progressLine, documentHeight, viewportHeight) {
+    let scroll = this.scrollTop(),
         progress = (scroll / (documentHeight - viewportHeight)) * 100;
 
     progressLine.style.width = progress + '%';
-  };
-})();
+    progressLine.style.height = this.height;
+    progressLine.style.backgroundColor = this.color;
+  }
+}
